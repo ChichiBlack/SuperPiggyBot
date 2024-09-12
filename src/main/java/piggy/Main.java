d8d4ebcd3c9a54147aa5d2da91094c7149d2b897
ChichiBlack <33328387+ChichiBlack@users.noreply.github.com>
2024-09-12 12:51:00 -0400

    Fix token

--- src/main/java/piggy/Main.java
+++ src/main/java/piggy/Main.java
@@ -4,12 +4,11 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import piggy.listeners.PingListener;
+import piggy.token.BotToken;
 
 public class Main {
     public static void main(String[] args) {
-        // MTI4MzgxOTE5NDI0MDA3Nzg5NA.GkncFF.i-3ulNiK3IFmqfCA7LCiZD49q96uUdxFwqDEss
-        String BOT_TOKEN = "MTI4MzgxOTE5NDI0MDA3Nzg5NA.GkncFF.i-3ulNiK3IFmqfCA7LCiZD49q96uUdxFwqDEss";
-        JDA api = JDABuilder.createDefault(BOT_TOKEN).enableIntents(GatewayIntent.MESSAGE_CONTENT).addEventListeners(new PingListener()).build();
+        JDA api = JDABuilder.createDefault(BotToken.BOT_TOKEN).enableIntents(GatewayIntent.MESSAGE_CONTENT).addEventListeners(new PingListener()).build();
 
     }
 }
